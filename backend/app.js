ca06b681675c3a34e40ceee239cf11973ba1595a
anuragkumar07070 <anuragkumar07070@gmail.com>
2026-02-19 10:45:09 +0530

    refactor backend structure by consolidating app initialization into app.js and removing index.js

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,32 @@
-// import express from "express";
-// import cors from "cors";
+import express from "express";
+import cors from "cors";
 
-// import authRoutes from "./routes/authRoutes.js";
-// import masterRoutes from "./routes/masterRoutes.js";
-// import shopRoutes from "./routes/shopRoutes.js";
-// import orderRoutes from "./routes/orderRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import masterRoutes from "./routes/masterRoutes.js";
+import shopRoutes from "./routes/shopRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 
-// const app = express();
+const app = express();
 
-// app.use(cors());
+app.use(cors());
 
-// app.use(cors({
-//   origin: "*",   // for testing
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// }));
+app.use(cors({
+  origin: "*",   // for testing
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// /* Routes */
-// app.use("/api/auth", authRoutes);
-// app.use("/api/master", masterRoutes);
-// app.use("/api/shop", shopRoutes);
-// app.use("/api/order", orderRoutes);
+/* Routes */
+app.use("/api/auth", authRoutes);
+app.use("/api/master", masterRoutes);
+app.use("/api/shop", shopRoutes);
+app.use("/api/order", orderRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("API Running 🚀");
-// });
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
 
-// export default app;
+export default app;
